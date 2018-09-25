1834b8084bc0dfb27771ac76d0dbbaec35c780bb
felipesml <carlos.sanmartin@trifenix.com>
2018-09-24 22:05:00 -0300

    actualiza url youtube

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -133,7 +133,7 @@ const Footer = ({ children }) => {
               <a href="https://www.linkedin.com/company/trifenix" target="_blank" rel="noopener noreferrer">
                 <img src="https://www.shareicon.net/download/2015/08/28/92041_in.svg"></img>
               </a>
-              <a href="https://www.youtube.com/trifenix_io" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.youtube.com/channel/UCfMZ12REWKSh-RGYmGhJxkA" target="_blank" rel="noopener noreferrer">
                 <img src="https://www.shareicon.net/download/2015/08/28/92023_video.svg"></img>
               </a>
            </EfectoLink>
