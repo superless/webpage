c648ea0c3f5dbc120141a0d56da4aac084796147
felipesml <carlos.sanmartin@trifenix.com>
2018-10-18 14:41:37 -0300

    creando archivo particulas

---
+++ src/utilities/ParticlesApp.jsx
@@ -0,0 +1,218 @@
+import React, { Component } from 'react'
+import Particles from 'react-particles-js';
+import { encodingExists } from 'iconv-lite';
+
+
+const particleOpt = {
+
+/*	BURBUJAS
+	"particles": {
+        "number": {
+            "value": 160,
+            "density": {
+                "enable": false
+            }
+        },
+        "size": {
+            "value": 2,
+            "random": true,
+            "anim": {
+                "speed": 4,
+                "size_min": 0.3
+            }
+        },
+        "line_linked": {
+            "enable": false
+        },
+        "move": {
+            "random": true,
+            "speed": 2,
+            "direction": "top",
+            "out_mode": "out"
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "bubble"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        },
+        "modes": {
+            "bubble": {
+                "distance": 250,
+                "duration": 2,
+                "size": 0,
+                "opacity": 0
+            },
+            "repulse": {
+                "distance": 400,
+                "duration": 2
+            }
+        }
+	}
+	*/
+
+/*ESTRELLAS
+	"particles": {
+		"number": {
+			"value": 100,
+			"density": {
+				"enable": true,
+				"value_area": 1500
+			}
+		},
+		"line_linked": {
+			"enable": true,
+			"opacity": 0.02
+		},
+		"move": {
+			"direction": "right",
+			"speed": 0.05
+		},
+		"size": {
+			"value": 1
+		},
+		"opacity": {
+			"anim": {
+				"enable": true,
+				"speed": 1,
+				"opacity_min": 0.05
+			}
+		}
+	},
+	"retina_detect": true
+*/
+
+/*PARTICULAS SIMPLES*/
+"particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "polygon",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 377.61590372012546,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+
+
+}
+
+
+class ParticlesApp extends Component{
+  
+    render(){
+        return (
+          <div>
+            <Particles
+            params={particleOpt} 
+            />
+        </div>
+        );
+    };
+
+}
+export default ParticlesApp
